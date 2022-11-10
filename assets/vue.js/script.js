202b4cd891d177970177735a241eab9d1c23d29a
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-10 17:11:14 +0100

    exercise done

--- assets/vue.js/script.js
+++ assets/vue.js/script.js
@@ -1,12 +1,26 @@
-var app = new Veu({
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, 
+utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, 
+presa anch’essa da un data.
+*/
+
+var app = new Vue({
 
     el: '#root',
 
     data: {
-
+        message: `Hey, I'm your first vue.js' sheet!!!!!!!`,
+        image: `https://img.freepik.com/premium-photo/bright-green-lawn-close-up-view-from-acid-green-color_96872-896.jpg?w=740`,
     },
 
     methods: {
-        
+        clickItAtAll: function(){
+            return this.bgc = ".green";
+        }
     }
 })
+
